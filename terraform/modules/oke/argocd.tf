1c2c75c8fdffe689c808780178303c1440f8fe0f
ealvesjr90 <ealvesjr90@gmail.com>
2026-03-20 09:43:53 -0300

    argo3

--- terraform/modules/oke/argocd.tf
+++ terraform/modules/oke/argocd.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">= 5.0.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.10.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+  }
+}
+
 resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
--- terraform/modules/queue/main.tf
+++ terraform/modules/queue/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">= 5.0.0"
+    }
+  }
+}
+
 resource "oci_queue_queue" "main" {
 
   compartment_id = var.compartment_id
