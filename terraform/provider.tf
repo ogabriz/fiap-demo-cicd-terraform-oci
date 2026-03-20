dcba3efb1ba34d47c9642b1e4afbf0e209296c5e
ealvesjr90 <ealvesjr90@gmail.com>
2026-03-20 09:23:25 -0300

    argocd1

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,7 +12,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.10"
+      version = ">= 2.10.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
@@ -33,18 +33,6 @@ data "oci_containerengine_cluster_kube_config" "oke" {
   cluster_id = module.oke.cluster_id
 }
 
-provider "kubectl" {
-  host                   = local.kubeconfig.clusters[0].cluster.server
-  cluster_ca_certificate = base64decode(local.kubeconfig.clusters[0].cluster.certificate-authority-data)
-  load_config_file       = false
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "oci"
-    args        = ["ce", "cluster", "generate-token", "--cluster-id", module.oke.cluster_id]
-  }
-}
-
 locals {
   kubeconfig = yamldecode(data.oci_containerengine_cluster_kube_config.oke.content)
 }
@@ -60,13 +48,27 @@ provider "kubernetes" {
   }
 }
 
-provider "helm" {
+provider "kubectl" {
   host                   = local.kubeconfig.clusters[0].cluster.server
   cluster_ca_certificate = base64decode(local.kubeconfig.clusters[0].cluster.certificate-authority-data)
+  load_config_file       = false
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "oci"
     args        = ["ce", "cluster", "generate-token", "--cluster-id", module.oke.cluster_id]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = local.kubeconfig.clusters[0].cluster.server
+    cluster_ca_certificate = base64decode(local.kubeconfig.clusters[0].cluster.certificate-authority-data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "oci"
+      args        = ["ce", "cluster", "generate-token", "--cluster-id", module.oke.cluster_id]
+    }
   }
 }
