33b9cd88168cc83810b641a355fd8888f12bf554
ealvesjr90 <ealvesjr90@gmail.com>
2026-04-16 19:24:45 -0300

    terra02

--- terraform/modules/oke/argocd.tf
+++ terraform/modules/oke/argocd.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.10.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+  }
+}
+
 resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
